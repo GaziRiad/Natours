be7f0bb5bbfb6b55a6c8e133bfdcb97d61566d1f
GaziRiad <ouhibimustapha36@gmail.com>
2024-10-15 11:27:04 +0100

    add custom validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -13,6 +13,7 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxLength: [40, "A tour name must have less or equal than 40 characters"],
       minLength: [10, "A tour name must have more or equal than 10 characters"],
+      // validate: [validator.isAlpha, "Tour name must only contain characters"],
     },
     slug: String,
     duration: {
@@ -47,7 +48,18 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, "A tour must have a price"],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        // eslint-disable-next-line object-shorthand
+        validator: function (value) {
+          // this only points to current doc on NEW document creation
+          return value <= this.price;
+        },
+        message:
+          "Discount price ({VALUE}) must be less or equal the regular price",
+      },
+    },
     summary: {
       type: String,
       required: [true, "A tour must have a summary"],
