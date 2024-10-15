95565512011c648d13e0345627764b49a66db4ac
GaziRiad <ouhibimustapha36@gmail.com>
2024-10-15 12:43:19 +0100

    Handling unhandled routes

--- app.js
+++ app.js
@@ -16,4 +16,14 @@ app.use(express.static(`${__dirname}/public`));
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+//Unamtched routes
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+
+  next();
+});
+
 module.exports = app;
