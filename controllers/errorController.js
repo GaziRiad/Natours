ccc381449a152a6a7a57c2d6a431250c4dbd98bc
GaziRiad <ouhibimustapha36@gmail.com>
2024-10-16 12:36:21 +0100

    Handling mongoose validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -41,6 +41,12 @@ const handleDuplicateFieldsDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Validation failed: ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
@@ -54,7 +60,7 @@ module.exports = (err, req, res, next) => {
     // Duplicate key error
     if (err.code === 11000) error = handleDuplicateFieldsDB(error);
 
-    // if (err.name === "ValidationError") error = handleValidationErrorDB(error);
+    if (err.name === "ValidationError") error = handleValidationErrorDB(error);
 
     sendErrorProd(error, res);
   }
