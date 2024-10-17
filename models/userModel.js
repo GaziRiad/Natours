93faa201218695fcb2f0eceeb6d97f16756f67a2
GaziRiad <ouhibimustapha36@gmail.com>
2024-10-17 13:13:47 +0100

    Managing passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,34 +1,51 @@
 /* eslint-disable import/no-extraneous-dependencies */
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const useSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "A user must have a name"],
-  },
-  email: {
-    type: String,
-    required: [true, "A use must have an email"],
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, "Please provide a valid email"],
-  },
-  photo: String,
-  password: {
-    type: String,
-    required: [true, "A use must have a password"],
-    min: [8, "A password should have atleast 8 characters"],
-  },
-  passwordConfirm: {
-    type: String,
-    validate: {
-      validator: function (value) {
-        return this.password === value;
+const useSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "A user must have a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "A use must have an email"],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email"],
+    },
+    photo: String,
+    password: {
+      type: String,
+      required: [true, "A use must have a password"],
+      minLength: [8, "A password should have atleast 8 characters"],
+    },
+    passwordConfirm: {
+      type: String,
+      validate: {
+        // This works only on CREATE and SAVE!!
+        validator: function (value) {
+          return this.password === value;
+        },
+        message: "Passwords needs to match",
       },
-      message: "Passwords needs to match",
     },
   },
+  { timestamps: true },
+);
+
+useSchema.pre("save", async function (next) {
+  //Only run this func if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  //Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // DELETE THE FIELD passwordConfirm
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", useSchema);
