383fca2c19ac2e363461467908873b2ac2c4d1e0
GaziRiad <ouhibimustapha36@gmail.com>
2024-10-25 16:23:40 +0100

    Sending JWT via http only cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,17 @@ const signinToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signinToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    secure: process.env.NODE_ENV !== "development",
+    httpOnly: true,
+  };
+
+  res.cookie("jwt", token, cookieOptions);
+
   res.status(statusCode).json({
     status: "success",
     token,
