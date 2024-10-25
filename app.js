58dff35370d7e5da882bb174566a1c86a130b5a5
GaziRiad <ouhibimustapha36@gmail.com>
2024-10-25 17:35:54 +0100

    Setting security HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const { rateLimit } = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./lib/utils/appError");
 const userRouter = require("./routes/userRoutes");
@@ -10,21 +11,27 @@ const globalErrorHandler = require("./controllers/errorController");
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 60 minutes
   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  // standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  // legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   message: "Too many requests from this IP, please try again in an hour!",
 });
 
 app.use("/api", limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", tourRouter);
