de917e0832f7e09f254c2e130f5686c09a991ce8
GaziRiad <ouhibimustapha36@gmail.com>
2024-10-27 23:33:33 +0100

    Modeling reviews: Parent referencing

---
+++ models/reviewModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+// review /  rating / createdAt / ref to tour / ref to user
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "Review cannot be empty!"],
+    },
+    rating: {
+      type: Number,
+      required: [true, "A review must have a rating"],
+      min: 1,
+      max: 5,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Review must belong to a tour"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user"],
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
